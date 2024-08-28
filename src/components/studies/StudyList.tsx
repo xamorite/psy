7e2ca998b58992da60bb368f8613eb0c8d8a58ff
Raiza <wisdomadebola62@gmail.com>
2024-08-28 01:59:41 +0100

    fix:better styling✨✨

--- src/components/studies/StudyList.tsx
+++ src/components/studies/StudyList.tsx
@@ -1,28 +1,33 @@
 import { Study } from "@/types/studyViewList";
+import Link from "next/link";
 
 interface studyListProps {
-  study: Study;
+    study: Study;
 }
 
 const StudyList = ({ study }: studyListProps) => {
-  // console.log(study);
+    // console.log(study);
 
-  return (
-    <div key={study.id} className="w-full my-5 text-xl">
-      <div className="">
-        {study.article_type.map((article, index) => (
-          <div key={index} className="text-[#5A3A31]">
-            {article.article_name}
-          </div>
-        ))}
-        {study.title}
-        <p className="text-muted-foreground">
-          {study.journal_name},{study.year}
-        </p>
-        <p className="text-muted-foreground">{study.lead_author}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div key={study.id} className="w-full flex flex-col gap-32 my-5">
+            <div className="max-w-4xl space-y-1">
+                {study.article_type.map((a, index) => (
+                    <div key={index} className="font-medium text-[#5A3A31]">
+                        {a.article_name}
+                    </div>
+                ))}
+                <Link
+                    href={""}
+                    className="text-xl font-semibold tracking-tight text-balance hover:text-gray-600">
+                    {study.title}
+                </Link>
+                <div className="text-gray-800">
+                    {study.journal_name},{" "}{study.year}
+                </div>
+                <p className="text-muted-foreground">{study.lead_author}</p>
+            </div>
+        </div>
+    );
 };
 
 export default StudyList;
