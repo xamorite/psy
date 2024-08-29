91b8254236a501e04e547169beafe9942b3dcbff
Raiza <wisdomadebola62@gmail.com>
2024-08-29 14:15:33 +0100

    fix:Better styling

--- src/hooks/use-get-studyView.tsx
+++ src/hooks/use-get-studyView.tsx
@@ -8,13 +8,13 @@ export const useGetStudyLists = () => {
     const query = useQuery<Study[], Error>({
         queryKey: ["study-lists"],
         queryFn: async () => {
-           const response = await axios.get("api/studyview")
+           const response = await axios.get("api/study-view")
 
             if(response.status === 500) {
                 throw new Error("Failed to fetch study list")
             }
 
-            console.log(response);
+            // console.log(response);
  
             return response.data;
         },
