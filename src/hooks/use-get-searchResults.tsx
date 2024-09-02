505a759a7f2284b8a7b6e2760ac3a8e65697cf88
Raiza <wisdomadebola62@gmail.com>
2024-09-02 21:05:58 +0100

    chore:Added new Props and changed the workings of the hooks

--- src/hooks/use-get-searchResults.tsx
+++ src/hooks/use-get-searchResults.tsx
@@ -2,23 +2,26 @@ import { ApiResponse } from "@/types/studyViewList";
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 
+interface SearchProps {
+    year: string
+    disorder: string
+    region: string
+    article: string
+}
 
 
-export const useGetSearchReasult = (searchTerm: string, debouncedSearchTerm: string) => {
+export const useGetSearchReasult = ( searchTerm: string, debouncedSearchTerm: string, pageNum: number, {year, region, disorder, article }: SearchProps) => {
     const query = useQuery<ApiResponse, Error>({
-        queryKey: ['searchResults', debouncedSearchTerm],
+        queryKey: ['searchResults', debouncedSearchTerm, year, region, disorder, article, pageNum],
         queryFn: async () => {
-            const response = await axios.get(`api/search-study?title=${searchTerm}`)
+            const response = await axios.get(`api/search-study?title=${searchTerm}&year=${year}&research_regions=${region}&disorder=${disorder}&article_type=${article}&page=${pageNum}`)
 
             if (response.status === 500) {
                 throw new Error("Failed to fetch search results")
             }
 
-            // console.log(response);
-
             return response.data;
         },
-        // enabled: !!searchTerm,
     });
 
     return query;
