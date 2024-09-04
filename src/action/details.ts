22a3d3f51ebc555412508df254bad10fc1e1d126
Raiza <wisdomadebola62@gmail.com>
2024-09-04 04:28:24 +0100

    chore:Better Styling

--- src/action/details.ts
+++ src/action/details.ts
@@ -1,14 +1,12 @@
 import axios from "axios";
 
 export const getDetails = async (id: number) => {
-    try {
-        
-        const res = await axios.get(`https://algorithmxcomp.pythonanywhere.com/api/studies/${id}`)
-        console.log(res.data)
+    try { 
+
+        const res = await axios.get(`https://algorithmxcomp.pythonanywhere.com/api/studies/${id}`);
         return res.data;
-       
+        
     } catch (error) {
-        console.error('Error fetching data:', error)
        return error;
     }
 };
