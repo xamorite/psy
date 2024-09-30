9c86b5665a94d6d812e4781f76744cf48da39fc3
Raiza <wisdomadebola62@gmail.com>
2024-09-30 21:56:30 +0100

    chore: include length in the structure

--- src/types/suggestion.ts
+++ src/types/suggestion.ts
@@ -1,6 +1,6 @@
 
 export interface Suggestion {
     authors: string[]
-    disorders: { id: number; disorder_name: string }[]
+    disorders: { id: number; disorder_name: string; length: number }[]
     study_titles: string[]
 }
